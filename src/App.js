4d58b4bf3eb7b03042f5fa761403b793e27dc212
Ross Condie <rosscondie@gmail.com>
2023-03-10 16:13:47 +0100

    rendered header component to container

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import RickAndMortyContainer from "./containers/RickAndMortyContainer";
 function App() {
   return (
     <div className="App">
-      <h1>Rick & Morty Character Finder</h1>
       <RickAndMortyContainer />
     </div>
   );
--- src/containers/RickAndMortyContainer.js
+++ src/containers/RickAndMortyContainer.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Header from "../components/Header";
+
 
 function RickAndMortyContainer() {
     return (
-        <h2>Hello</h2>
+        <>
+        <Header />
+        </>
     )
 }
 
