f2bdbd46b5df2c5c8540c0ed4816ffea1652ebb5
Ross Condie <rosscondie@gmail.com>
2023-03-10 16:18:17 +0100

    rendered characterlist component to container

---
+++ src/components/CharacterList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function CharacterList() {
+    return (
+        <h3>Character List</h3>
+    )
+}
+
+export default CharacterList;
--- src/containers/RickAndMortyContainer.js
+++ src/containers/RickAndMortyContainer.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
+import CharacterList from "../components/CharacterList";
 
 
 function RickAndMortyContainer() {
     return (
         <>
         <Header />
+        <CharacterList />
         </>
     )
 }
